d45b6a0e1bcec5b85511091c8d923a06d10b6502
Will Bittner <will@brookfield.ventures>
2015-04-01 09:46:01 -0400

    cleaning

--- dist/scripts/ang-material.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module( 'ngApp', [ 'ngMaterial' ] );
-app.controller( 'AppCtrl', [ '$scope', '$mdSidenav', function ( $scope, $mdSidenav ) {
-	$scope.toggleSidenav = function ( menuId ) {
-		$mdSidenav( menuId ).toggle();
-	};
-} ] );
