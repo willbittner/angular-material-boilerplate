0192fee5eb5937376e902be84fc0efa34faf0d06
willbittner <will@brookfield.vc>
2015-04-02 23:20:46 -0400

    Updated gulp to autobuild and reload on changes
    I need to rename directory structure btw.

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,31 @@ var gulp = require( 'gulp' );
 var sass = require( 'gulp-ruby-sass' );
 var browserSync = require( 'browser-sync' );
 var rename = require( 'gulp-rename' );
-var uglify = require( 'gulp-uglify' );
 var merge = require( 'merge-stream' );
-var gulpBowerFiles = require( 'gulp-bower-files' );
+var mainBowerFiles = require( 'main-bower-files' );
 var reload = browserSync.reload;
-gulp.task( 'sass', function () {
-	return sass( 'src/scss/styles.scss' ).pipe( gulp.dest( 'dist/css' ) ).pipe( reload( {
-		stream: true
-	} ) );
-} );
+var LIVERELOAD_PORT = 35729;
+var less = require( 'gulp-less' );
+var livereload = require( 'gulp-livereload' );
 gulp.task( 'build', function () {
-	var htmlg = gulp.src( 'src/**/*.html' ).pipe( gulp.dest( 'dist' ) );
-	var cssg = gulp.src( 'src/css/**/*.css' ).pipe( gulp.dest( 'dist/css' ) );
-	// This will output the non-minified version
-	var js = gulp.src( 'src/scripts/**/*.js' )
-		// This will output the non-minified version
-		.pipe( gulp.dest( 'dist/scripts' ) )
-		// This will minify and rename to foo.min.js
-		.pipe( uglify() ).pipe( rename( {
-			extname: '.min.js'
-		} ) ).pipe( gulp.dest( 'dist/scripts' ) );
-	return merge( htmlg, cssg, js );
+	gulp.src( 'src/less/*.less' ).pipe( less() ).pipe( gulp.dest( 'src/css' ) ).pipe( livereload() );
+	gulp.src( 'src/css/**/*.css' ).pipe( gulp.dest( 'dist/css' ) ).pipe( livereload() );
+	gulp.src( 'src/**/*.html' ).pipe( gulp.dest( 'dist/' ) ).pipe( livereload() );
+	gulp.src( 'src/scripts/**/*.js' ).pipe( gulp.dest( 'dist/scripts' ) ).pipe( livereload() );
+	sass( 'src/scss/styles.scss' ).pipe( gulp.dest( 'dist/css' ) ).pipe( livereload() );
 } );
 // watch files for changes and reload
 gulp.task( "bower-files", function () {
-	gulpBowerFiles().pipe( gulp.dest( "dist/lib" ) );
+	gulp.src( mainBowerFiles() ).pipe( gulp.dest( "dist/lib" ) );
 } );
-gulp.task( 'default', [ 'build', 'sass', 'bower-files' ], function () {
+gulp.task( 'default', [ 'bower-files', 'build' ], function () {
 	browserSync( {
 		server: {
 			baseDir: 'dist'
 		}
 	} );
-	gulp.watch( 'src/scss/*.scss', [ 'sass' ] );
-	gulp.watch( [ 'src/*.html', 'src/css/**/*.css', 'scripts/**/*.js' ], {
-		cwd: 'dist'
-	}, reload );
+	gulp.watch( 'src/scss/*.scss', [ 'build' ] );
+	gulp.watch( 'src/less/*.less', [ 'build' ] );
+	gulp.watch( [ 'src/*.html', 'src/**/*.js' ], [ 'build' ] );
+	livereload.listen( LIVERELOAD_PORT );
 } );
